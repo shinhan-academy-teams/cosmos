5c64997cdfc3d988d3aa65c0edea2dbf9bfa4a66
zzahee <zzahee366@gmail.com>
2023-04-18 10:39:11 +0900

    feat(BE): CommonControllerInterface, FrontController, SignUpController생성

---
+++ src/main/java/com/cosmos/controller/FrontController.java
@@ -0,0 +1,55 @@
+package com.cosmos.controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/FrontController")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public FrontController() {
+        super();
+    }
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String path = request.getServletPath();
+		CommonControllerInterface controller = null;
+		Map<String, Object> data = new HashMap<>();
+		data.put("method", request.getMethod());  //get인지 post인지 모르니까 받아옴
+		data.put("request", request);
+		
+		switch(path) {
+		case "/signup":
+			controller = new SignUpController();
+			break;
+		default:
+			break;
+		}
+		String page = null;
+		try {
+			page = controller.execute(data);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		RequestDispatcher rd;
+		rd = request.getRequestDispatcher(page);
+		rd.forward(request, response);
+		
+	}
+
+	
+	
+
+}
---
+++ src/main/java/com/cosmos/controller/SignUpController.java
@@ -0,0 +1,13 @@
+package com.cosmos.controller;
+
+import java.util.Map;
+
+public class SignUpController implements CommonControllerInterface {
+
+	@Override
+	public String execute(Map<String, Object> data) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
