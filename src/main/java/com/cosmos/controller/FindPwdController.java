38cdeb825e3b36a93f99852a1b1b5f30fbe48b1f
zzahee <zzahee366@gmail.com>
2023-04-19 20:07:30 +0900

    feat(BE): 이메일 인증 구현

--- src/main/java/com/cosmos/controller/FindPwdController.java
+++ src/main/java/com/cosmos/controller/FindPwdController.java
@@ -18,12 +18,14 @@ public class FindPwdController implements CommonControllerInterface {
 		}else {
 			HttpServletRequest request = (HttpServletRequest)data.get("request");
 			String id = request.getParameter("id");  //아이디 가져옴
+			String email = request.getParameter("email");//이메일 가져옴
 			FindService service = new FindService(); 
 			MemberVO vo = service.findById(id);//아이디로 해당 데이터 찾기
-			MemberVO rndvo= service.updateRndPwd(vo);  //해당 데이터를 업데이트하기
-		
+			service.updateRndPwd(vo);  //해당 데이터를 업데이트하기
 		}
 		return "find-pw.jsp";
 	}
 
+	
+	
 }
