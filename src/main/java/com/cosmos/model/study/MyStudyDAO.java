ae2ba3f64d691d5b67b76c34cd8974321b7b854c
0seony <yseon406@gmail.com>
2023-04-21 17:33:15 +0900

    feat(BE): 내 스터디 메인

--- src/main/java/com/cosmos/model/study/MyStudyDAO.java
+++ src/main/java/com/cosmos/model/study/MyStudyDAO.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.cosmos.util.OracleUtil;
+import com.cosmos.vo.QuizVO;
 import com.cosmos.vo.StudyGroupVO;
 
 // 내 스터디 그룹 관련
@@ -42,6 +43,33 @@ public class MyStudyDAO {
 		}
 		return studyGroup;
 	
+	}
+	
+	// 내 스터디 문제 목록 보여주기
+	public List<QuizVO> showAllQuiz(int studyNO){
+		// 쿼리문
+		String sql = "select * from quiz where sg_no=" + studyNO;
+		List<QuizVO> allQuiz = new ArrayList<>();
+		conn = OracleUtil.getConnection();
+		try {
+			statement = conn.createStatement();
+			result = statement.executeQuery(sql);
+			while (result.next()) {
+				QuizVO quiz = new QuizVO();
+				quiz.setQuiz_no(result.getInt("quiz_no"));
+				quiz.setSg_no(result.getInt("sg_no"));
+				quiz.setQuiz_url(result.getString("quiz_url"));
+				quiz.setQuiz_title(result.getString("quiz_title"));
+				quiz.setQuiz_date(result.getDate("quiz_date"));
+				allQuiz.add(quiz);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			OracleUtil.dbDisconnect(result, statement, conn);
+		}
+		return allQuiz;
+		
 	}
 	
 
