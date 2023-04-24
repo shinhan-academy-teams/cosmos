69bbcbf70df73c7b2476c6ea32cfe8bdeafb6f72
zzahee <zzahee366@gmail.com>
2023-04-24 19:56:00 +0900

    feat(BE) : 코드 보기 기능 완료

--- src/main/java/com/cosmos/model/code/CodeDAO.java
+++ src/main/java/com/cosmos/model/code/CodeDAO.java
@@ -4,8 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.cosmos.util.OracleUtil;
+import com.cosmos.vo.CodeVO;
 
 public class CodeDAO {
 
@@ -33,5 +36,38 @@ public class CodeDAO {
 			OracleUtil.dbDisconnect(rs, st, conn);
 		}
 		return result;
+	}
+	
+	//코드 보기
+	public List<CodeVO> showCode(int quizNo, int sgNo) {
+		String sql = "select * from code where quiz_no =? and sg_no=?"; 
+		conn = OracleUtil.getConnection();
+		List<CodeVO> codes = new ArrayList<>();
+		
+		try {
+			st = conn.prepareStatement(sql);
+			st.setInt(1, quizNo);
+			st.setInt(2, sgNo);
+			rs = st.executeQuery();
+			while(rs.next()) {
+				CodeVO code = new CodeVO();
+				code.setCode_no(rs.getInt("code_no"));
+				code.setQuiz_no(rs.getInt("quiz_no"));
+				code.setSg_no(rs.getInt("sg_no"));
+				code.setCode_content(rs.getString("code_content"));
+				//System.out.println(rs.getString("code_content"));
+				code.setCode_date(rs.getDate("code_date"));
+				code.setMember_no(rs.getInt("member_no"));
+				code.setCode_lang(rs.getString("code_lang"));
+				codes.add(code);
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			OracleUtil.dbDisconnect(rs, st, conn);
+		}
+		return codes;
 	}
 }
