3a36fe81ede32767118499583f531a854719ddc1
zzahee <zzahee366@gmail.com>
2023-04-26 13:58:48 +0900

    feat(BE) : 댓글 select 기능 구현

--- src/main/java/com/cosmos/controller/study/mystudy/ShowCodeController.java
+++ src/main/java/com/cosmos/controller/study/mystudy/ShowCodeController.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpSession;
 
 import com.cosmos.controller.CommonControllerInterface;
 import com.cosmos.model.code.CodeService;
+import com.cosmos.model.comments.CommentsService;
 import com.cosmos.model.mark.MarkService;
 import com.cosmos.vo.MemberVO;
 import com.cosmos.vo.code.MarkCodeVO;
@@ -20,16 +21,23 @@ public class ShowCodeController implements CommonControllerInterface {
 	@Override
 	public String execute(Map<String, Object> data) throws Exception {
 		HttpServletRequest request = (HttpServletRequest)data.get("request");
+		
+		//코드
 		CodeService codeService = new CodeService();
 		int quizNo = Integer.parseInt(request.getParameter("quizno"));
 		int sgNo = Integer.parseInt(request.getParameter("studyno"));
 		List<MarkCodeVO> allCode = codeService.showCode(quizNo, sgNo);
 		request.setAttribute("allCode", allCode);
 		
+		//좋아요
 		HttpSession session = request.getSession();
 		MemberVO nowUser = (MemberVO) session.getAttribute("user");
 		MarkService markService = new MarkService();
 		request.setAttribute("markList", markService.selectMarkByMember(nowUser.getMember_no()));
+		
+		//댓글
+		CommentsService cmtService = new CommentsService();
+		request.setAttribute("cmtMap", cmtService.selectCmtByCode(quizNo,sgNo));
 		
 		return "codeReview.jsp";
 	}
---
+++ src/main/java/com/cosmos/vo/comments/CmtJoinMemVO.java
@@ -0,0 +1,27 @@
+package com.cosmos.vo.comments;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CmtJoinMemVO extends CommentsVO {
+	private String member_name;
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("{" + super.toString())
+			.append(", member_name=")
+			.append(member_name + "}");
+		return builder.toString();
+	}
+
+	
+	
+	
+}
